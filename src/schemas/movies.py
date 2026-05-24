c92b7959e4c04b5d69a31ec37f89455576ecaea9
Andriy Shafran <shafran.andriy@gmail.com>
2026-05-24 14:39:50 +0300

    fix: Fixed schemas to schemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,13 +1,9 @@
 import datetime
+from typing import Optional
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
-
-class MovieModel(BaseModel):
-    genre: str
-    crew: str
-    orig_title: str
-    orig_lang: str
+from database.models import MovieStatusEnum
 
 
 class MovieListItemSchema(BaseModel):
@@ -22,42 +18,52 @@ class MovieListItemSchema(BaseModel):
 
 class MovieListResponseSchema(BaseModel):
     movies: list[MovieListItemSchema]
-    prev_page: str | None
-    next_page: str | None
+    prev_page: Optional[str]
+    next_page: Optional[str]
     total_pages: int
     total_items: int
 
 
 class CountrySchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     code: str
-    name: str
+    name: Optional[str]
 
 
 class GenreSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     name: str
 
 
 class ActorSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     name: str
 
 
 class LanguageSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     name: str
 
 
 class MovieDetailResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     name: str
     date: datetime.date
     score: float
     overview: str
-    status: str
-    budget: int
-    revenue: int
+    status: MovieStatusEnum
+    budget: float
+    revenue: float
     country: CountrySchema
     genres: list[GenreSchema]
     actors: list[ActorSchema]
@@ -65,24 +71,24 @@ class MovieDetailResponseSchema(BaseModel):
 
 
 class MovieCreateSchema(BaseModel):
-    name: str
+    name: str = Field(max_length=255)
     date: datetime.date
-    score: float
+    score: float = Field(ge=0, le=100)
     overview: str
-    status: str
-    budget: int
-    revenue: int
-    country: CountrySchema
-    genres: list[GenreSchema]
-    actors: list[ActorSchema]
-    languages: list[LanguageSchema]
+    status: MovieStatusEnum
+    budget: float = Field(ge=0)
+    revenue: float = Field(ge=0)
+    country: str
+    genres: list[str]
+    actors: list[str]
+    languages: list[str]
 
 
 class MovieUpdateSchema(BaseModel):
-    name: str
-    date: datetime.date
-    score: float
-    overview: str
-    status: str
-    budget: int
-    revenue: int
+    name: Optional[str] = Field(default=None, max_length=255)
+    date: Optional[datetime.date] = None
+    score: Optional[float] = Field(default=None, ge=0, le=100)
+    overview: Optional[str] = None
+    status: Optional[MovieStatusEnum] = None
+    budget: Optional[float] = Field(default=None, ge=0)
+    revenue: Optional[float] = Field(default=None, ge=0)
