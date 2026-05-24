f6f58de49cebf6e7e576e38a2f24e28b4e700946
Andriy Shafran <shafran.andriy@gmail.com>
2026-05-24 14:30:08 +0300

    fix: Fixed schemas to schemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -3,27 +3,86 @@ import datetime
 from pydantic import BaseModel, ConfigDict
 
 
-class MovieDetailResponseSchema(BaseModel):
+class MovieModel(BaseModel):
+    genre: str
+    crew: str
+    orig_title: str
+    orig_lang: str
+
+
+class MovieListItemSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
     name: str
     date: datetime.date
     score: float
-    genre: str
     overview: str
-    crew: str
-    orig_title: str
-    status: str
-    orig_lang: str
-    budget: float
-    revenue: float
-    country: str
 
 
 class MovieListResponseSchema(BaseModel):
-    movies: list[MovieDetailResponseSchema]
+    movies: list[MovieListItemSchema]
     prev_page: str | None
     next_page: str | None
     total_pages: int
     total_items: int
+
+
+class CountrySchema(BaseModel):
+    id: int
+    code: str
+    name: str
+
+
+class GenreSchema(BaseModel):
+    id: int
+    name: str
+
+
+class ActorSchema(BaseModel):
+    id: int
+    name: str
+
+
+class LanguageSchema(BaseModel):
+    id: int
+    name: str
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    status: str
+    budget: int
+    revenue: int
+    country: CountrySchema
+    genres: list[GenreSchema]
+    actors: list[ActorSchema]
+    languages: list[LanguageSchema]
+
+
+class MovieCreateSchema(BaseModel):
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    status: str
+    budget: int
+    revenue: int
+    country: CountrySchema
+    genres: list[GenreSchema]
+    actors: list[ActorSchema]
+    languages: list[LanguageSchema]
+
+
+class MovieUpdateSchema(BaseModel):
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    status: str
+    budget: int
+    revenue: int
